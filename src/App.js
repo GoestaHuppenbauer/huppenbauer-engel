d2d5c28455c5a5ebe8d1d663dd01ebf6407d08b1
Gösta Huppenbauer <40520263+GoestaHuppenbauer@users.noreply.github.com>
2020-05-30 15:14:34 +0200

    new images and adress

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <BootstrapCarouselDemo></BootstrapCarouselDemo>  
     
     <p className="centered"><span className="Überschrift">WIR PLANEN LEBENSRÄUME</span><br></br>Wenn sie mehr erfahren wollen melden Sie sich bei uns.</p>
-    <p className="bottom-right"><span className="name">Architekturbüro Huppenbauer und Engel</span><br></br>Kochenmühle 2<br></br>D-70771 Leinfelden-Echterdingen</p>
+    <p className="bottom-right"><span className="name">Architekturbüro Huppenbauer und Engel</span><br></br>Kochenmühle 2<br></br>D-70771 Leinfelden-Echterdingen<br></br><br></br>07157 / 737930<br></br>info@huppenbauer-engel.de</p>
     </div>
     </div>
   );  
--- src/carousel.js
+++ src/carousel.js
@@ -3,15 +3,20 @@ import Carousel from 'react-bootstrap/Carousel'
 import img1 from './imgs/field.jpg';
 import img2 from './imgs/field2.jpg';
 import img3 from './imgs/flowers.jpg';
-import img4 from './imgs/stairway.jpg';
 import Image from 'react-bootstrap/Image'
+import img5 from './imgs/turkey.jpg';
+import img6 from './imgs/sea2.jpg';
+import img8 from './imgs/mountains.jpg';
+import img9 from './imgs/road66.jpg';
+import img4 from './imgs/newsealand.jpg';
+
 export class BootstrapCarouselDemo extends Component {  
         render() {  
                 return (  
                         <div>  
                          
                           
-                        <Carousel fade={true} interval={4000} keyboard={true} pauseOnHover={false} indicators={true} controls={true}>  
+                        <Carousel fade={true} interval={4000} keyboard={true} pauseOnHover={false} indicators={false} controls={true}>  
                         
                         <Carousel.Item style={{'height':"100vh"}}>  
                             <Image src={img1} fluid /> 
@@ -26,8 +31,26 @@ export class BootstrapCarouselDemo extends Component {
                         </Carousel.Item> 
 
                         <Carousel.Item style={{'height':"100vh"}}>  
+                            <Image src={img5} fluid /> 
+                        </Carousel.Item>
+
+                        <Carousel.Item style={{'height':"100vh"}}>  
+                            <Image src={img6} fluid /> 
+                        </Carousel.Item>
+
+                        <Carousel.Item style={{'height':"100vh"}}>  
                             <Image src={img4} fluid /> 
                         </Carousel.Item>  
+
+                        <Carousel.Item style={{'height':"100vh"}}>  
+                            <Image src={img8} fluid /> 
+                        </Carousel.Item> 
+
+                        <Carousel.Item style={{'height':"100vh"}}>  
+                            <Image src={img9} fluid /> 
+                        </Carousel.Item>  
+
+                       
                                     
                         </Carousel>  
                                 </div>  

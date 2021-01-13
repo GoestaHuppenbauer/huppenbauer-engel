33f1b22536894f6883fd04aae146ffceca5187d0
Gösta Huppenbauer <40520263+GoestaHuppenbauer@users.noreply.github.com>
2021-01-13 21:06:29 +0100

    build now

---
+++ build/precache-manifest.0ba1ae0008740825a78fc5ec09e5ff05.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b1b5009f4ca0206fbc2e519914de68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5f95c531a89a8eb77ba",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6d61f5ada4760eadd4ff",
+    "url": "/static/css/main.c06e13d7.chunk.css"
+  },
+  {
+    "revision": "f5f95c531a89a8eb77ba",
+    "url": "/static/js/2.96aeab8e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.96aeab8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d61f5ada4760eadd4ff",
+    "url": "/static/js/main.9fa5c01f.chunk.js"
+  },
+  {
+    "revision": "8a418ce51d069a0521f7",
+    "url": "/static/js/runtime-main.6b74088f.js"
+  },
+  {
+    "revision": "4c0d8bea7f2057cbffb908d1638c1970",
+    "url": "/static/media/desert.4c0d8bea.jpg"
+  },
+  {
+    "revision": "10f7762730b942da11365f90f69b01db",
+    "url": "/static/media/field.10f77627.jpg"
+  },
+  {
+    "revision": "0ff270e8faa48df7e2e61d3745c7623e",
+    "url": "/static/media/field2.0ff270e8.jpg"
+  },
+  {
+    "revision": "7af931d63f105366db47736e2c2e8b0e",
+    "url": "/static/media/flowers.7af931d6.jpg"
+  },
+  {
+    "revision": "ab4b02e29a2037ba3672692765fbaeb8",
+    "url": "/static/media/forest2.ab4b02e2.jpg"
+  },
+  {
+    "revision": "d1e58b139d0c577d452cbce55e0b5ba6",
+    "url": "/static/media/mountains.d1e58b13.jpg"
+  },
+  {
+    "revision": "4bdf11b3df678e3971c1d86374d96773",
+    "url": "/static/media/newsealand.4bdf11b3.jpg"
+  },
+  {
+    "revision": "6f2e37d45f145ef3c49b1771219bd425",
+    "url": "/static/media/road.6f2e37d4.jpg"
+  },
+  {
+    "revision": "d709e70adefb474264159b19420995b5",
+    "url": "/static/media/road66.d709e70a.jpg"
+  },
+  {
+    "revision": "ff9194c2214a044d5ab967a4852edebf",
+    "url": "/static/media/sea2.ff9194c2.jpg"
+  },
+  {
+    "revision": "bbd0d049b2170083e257a922f9a3e85e",
+    "url": "/static/media/sky.bbd0d049.jpg"
+  },
+  {
+    "revision": "7c6a8a29b6ec0643fee4696b4d16671b",
+    "url": "/static/media/turkey.7c6a8a29.jpg"
+  }
+]);
